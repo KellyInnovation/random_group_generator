090880b862ed926e1559b394d5fbcb09a1e44e0b
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-09-12 16:53:23 -0400

    Working random generator.

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
+from os import path
+
 from flask import render_template
 
 from app import app
+from app.lib.random_groups import RandomGroups
 
 @app.route("/")
 @app.route("/index")
 def index():
-
-    return render_template("index.html")
+	groups = RandomGroups()
+	
+	return render_template("index.html", groups=groups.teamed_groups)
